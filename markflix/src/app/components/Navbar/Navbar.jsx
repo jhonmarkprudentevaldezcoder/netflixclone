6d2b9eeebbbd15b7ec319ff9937dfb36161a3b64
jhon mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-04-13 03:29:01 +0800

    update

--- markflix/src/app/components/Navbar/Navbar.jsx
+++ markflix/src/app/components/Navbar/Navbar.jsx
@@ -12,15 +12,23 @@ export const Navbar = () => {
           </div>
         </div>
 
-        <div className="hidden  md:flex text-gray-300 py-4 px-10 space-x-6 ">
-          <h3 className="py-1">UNLIMITED TV SHOWS & MOVIES</h3>
-          <Link
-            target="_blank"
-            href="https://portfolio-seven-alpha-27.vercel.app/?fbclid=IwAR1E7pjDSvj7Ic2lWNlYiDiy2mByGZwCQrD1s796cDYqaknf01MOucRxiYY"
-            className="bg-red-800 px-10 py-1 cursor-pointer rounded-sm hover:bg-red-700 hover:text-gray-100 transition ease-in-out delay-75 duration-200"
-          >
-            JOIN US
-          </Link>
+        <div className=" text-gray-300 py-4 px-10 space-x-6 ">
+          <ul className="flex flex-row gap-4 content-center text-center">
+            <li>
+              <h3 className="py-1 hidden  md:flex">
+                UNLIMITED TV SHOWS & MOVIES
+              </h3>
+            </li>
+            <li>
+              <Link
+                target="_blank"
+                href="https://portfolio-seven-alpha-27.vercel.app/?fbclid=IwAR1E7pjDSvj7Ic2lWNlYiDiy2mByGZwCQrD1s796cDYqaknf01MOucRxiYY"
+                className="bg-red-800 px-8 md:px-8 py-1 cursor-pointer rounded-sm hover:bg-red-700 hover:text-gray-100 transition ease-in-out delay-75 duration-200"
+              >
+                JOIN US
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
